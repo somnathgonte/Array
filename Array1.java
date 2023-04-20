47f323c8108441b0a1b83782678e19dded6493f1
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-04-21 00:20:20 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,20 @@
+//Display all the elements of given array.
+
+public class Array
+{
+    public static void main(String []arg)
+    {
+        int a[]={11,21,51,101,151};
+        for(int i=0; i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+        //Or
+
+        for(float x:a)
+        {
+            System.out.println(x);
+        }
+    }
+}
